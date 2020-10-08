6f0eb2ede2893cd7277957816b759d963547cdc7
Hasan <hasan@gmail.com>
2020-10-08 23:33:52 +0600

    added backgroud image

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/hasan/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/hasan/Desktop/Flutter_Project/Clima-Flutter-master"
+export "FLUTTER_ROOT=/Users/hasan/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hasan/Flutter_Project/Weather-App"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/hasan/flutter/bin/cache/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hasan/Development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
